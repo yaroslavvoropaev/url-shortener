b5fb273e72b596c025f2fec12a54a535db7756c2
voropaev.ii <voropaevaroslav37@gmail.com>
2026-05-27 20:16:46 +0300

    change urlApplication

--- src/main/java/com/urlshortener/url_shortener/UrlShortenerApplication.java
+++ src/main/java/com/urlshortener/url_shortener/UrlShortenerApplication.java
@@ -8,5 +8,7 @@ public class UrlShortenerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerApplication.class, args);
+		System.out.println();
+		String
 	}
 }
